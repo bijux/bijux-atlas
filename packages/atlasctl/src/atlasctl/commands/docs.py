3b15aa096528e32989051ba4d5127acd4e0b01f7
bijan <mousavi.bijan@gmail.com>
2026-02-21 00:15:19 +0100

    refactor(commands): remove unused wrapper modules to avoid duplicate concepts

--- packages/atlasctl/src/atlasctl/commands/docs.py
+++
@@ -1,5 +0,0 @@
-from __future__ import annotations
-
-from ..docs.command import run_docs_command
-
-__all__ = ["run_docs_command"]
--- packages/atlasctl/src/atlasctl/commands/ops.py
+++
@@ -1,5 +0,0 @@
-from __future__ import annotations
-
-from ..ops.command import run_ops_command
-
-__all__ = ["run_ops_command"]
