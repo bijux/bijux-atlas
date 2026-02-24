fbd5bf9130f8ac821d68ed6be41d11685cafb3e1
bijan <mousavi.bijan@gmail.com>
2026-02-24 02:15:40 +0100

    refactor(checks): flatten internal registry checks into domain module

--- packages/atlasctl/src/atlasctl/checks/domains/internal/__init__.py
+++
@@ -1,8 +0,0 @@
-from __future__ import annotations
-
-from ...model import CheckDef
-from .checks import CHECKS as INTERNAL_CHECKS
-
-CHECKS: tuple[CheckDef, ...] = tuple(INTERNAL_CHECKS)
-
-__all__ = ["CHECKS"]
